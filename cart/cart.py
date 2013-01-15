eb538c737762b8e6e851f5933648caea8b0ad1b0
inlanger <inlanger@gmail.com>
2013-01-15 15:41:08 +0200

    Update module to new version. Change quantity validation in add function. Create cart_total function.

--- cart/cart.py
+++ cart/cart.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import datetime
 import models
 
 CART_ID = 'CART-ID'
 
-class ItemAlreadyExists(Exception):
-    pass
 
 class ItemDoesNotExist(Exception):
     pass
@@ -14,7 +15,7 @@ class Cart:
         cart_id = request.session.get(CART_ID)
         if cart_id:
             try:
-                cart = models.Cart.objects.get(id=cart_id, checked_out=False)
+                cart = models.Cart.objects.get(id=cart_id, owner=request.user, checked_out=False)
             except models.Cart.DoesNotExist:
                 cart = self.new(request)
         else:
@@ -25,13 +26,26 @@ class Cart:
         for item in self.cart.item_set.all():
             yield item
 
+    # get cart total function
+    def get_total(self):
+        result = 0
+        for item in self.cart.item_set.all():
+            result += item.total_price
+        return result
+
     def new(self, request):
-        cart = models.Cart(creation_date=datetime.datetime.now())
+        cart = models.Cart(owner=request.user, creation_date=datetime.datetime.now())
         cart.save()
         request.session[CART_ID] = cart.id
         return cart
 
-    def add(self, product, unit_price, quantity=1):
+    def add(self, product, unit_price, quantity):
+        # try to get valid quantity value
+        try:
+            quantity = int(quantity)
+        except:
+            quantity = 1
+
         try:
             item = models.Item.objects.get(
                 cart=self.cart,
@@ -45,14 +59,17 @@ class Cart:
             item.quantity = quantity
             item.save()
         else:
-            raise ItemAlreadyExists
-
-    def remove(self, product):
-        try:
             item = models.Item.objects.get(
                 cart=self.cart,
                 product=product,
             )
+            # if product in cart and user try to add more than 1 item
+            item.quantity += quantity
+            item.save()
+
+    def remove(self, item):
+        try:
+            item = models.Item.objects.get(cart=self.cart, product=item)
         except models.Item.DoesNotExist:
             raise ItemDoesNotExist
         else:
@@ -68,6 +85,10 @@ class Cart:
             raise ItemDoesNotExist
 
     def clear(self):
-        for item in self.cart.item_set:
+        for item in self.cart.item_set.all():
             item.delete()
 
+    def checkout(self):
+        self.cart.checked_out = True
+        self.cart.save()
+        return True
